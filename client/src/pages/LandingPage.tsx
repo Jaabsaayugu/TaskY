3f01aa301188516cb6ef83d68fc732495d21192d
unknown <jabezayugu04@gmail.com>
2025-07-17 23:06:22 +0300

    first commit

---
+++ client/src/pages/LandingPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Typography } from '@mui/material'
+
+function LandingPage() {
+  return (
+    <>
+    <Typography>Tasky The Page</Typography>
+    </>
+  )
+}
+
+export default LandingPage
